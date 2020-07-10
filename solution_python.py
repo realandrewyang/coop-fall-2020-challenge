c97cbb388414aa9988f63ff3b7d6826bc6ab4164
realandrewyang <andrew.yang.school@gmail.com>
2020-07-10 13:34:10 -0400

    solution

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,35 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.stack = []
+        self.undone = []
 
     def add(self, num: int):
-        pass
+        self.stack.append(num)
+        self.value += num
 
     def subtract(self, num: int):
-        pass
+        self.stack.append(-1 * num)
+        self.value -= num
 
     def undo(self):
-        pass
+        if self.stack:
+            self.undone.append(self.stack.pop())
+            self.value -= self.undone[-1]
 
     def redo(self):
-        pass
+        if self.undone:
+            self.stack.append(self.undone.pop())
+            self.value += self.stack[-1]
 
     def bulk_undo(self, steps: int):
-        pass
+        while self.stack and steps > 0:
+            self.undone.append(self.stack.pop())
+            self.value -= self.undone[-1]
+            steps -= 1
 
     def bulk_redo(self, steps: int):
-        pass
+        while self.undone and steps > 0:
+            self.stack.append(self.undone.pop())
+            self.value += self.stack[-1]
+            steps -= 1
